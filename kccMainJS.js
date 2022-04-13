ce16c520e5bc57aad3dddb463da51c5de061ea2d
EJACK95 <100305667+EJACK95@users.noreply.github.com>
2022-04-13 18:43:49 +0100

    Add files via upload

--- kccMainJS.js
+++ kccMainJS.js
@@ -1,9 +1,4 @@
 // function myName(inputs separated by comma){this is where you tell it what to do, every line of js needs to end in a ; return is the command to make the function show the value from the function};
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function will be useful
-console.log("Hello World")
-
-function movieDetails(title, released){
-    return `The movie was called $(title), it came out in $(released);
-}
-
+//https://developer.mozilla.org/en-US/docs/Web/API/Document 
 
